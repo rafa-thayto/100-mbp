d1477a5e4263ed452c900da044882d0405761ff3
Rafael Thayto <rafa.thayto@gmail.com>
2018-08-05 14:13:37 +0000

    Design created for the main page

--- mbp-front/src/components/Login/index.js
+++ mbp-front/src/components/Login/index.js
@@ -1,1 +1,22 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+
+const initialState = {
+  user_name: '',
+  password: ''
+}
+
+export default class Login extends Component {
+
+  state = { ...initialState }
+
+  constructor (props) {
+    super(props)
+
+  }
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
